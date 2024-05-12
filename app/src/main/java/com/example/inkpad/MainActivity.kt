db0046b881472ad0f1ac059f0baff0ddd0700a73
4rthur <shanukaamantha35@gmail.com>
2024-05-12 17:16:46 +0530

    commit 1

---
+++ app/src/main/java/com/example/inkpad/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.inkpad
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+import androidx.appcompat.app.AppCompatActivity
+import com.example.notesroompractice.R
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activiy_main)
+    }
+}
+
